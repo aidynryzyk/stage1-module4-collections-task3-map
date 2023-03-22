7bb97720e34702699659c883016d143a044fb976
aidyninho <ryzyk.aidyn@gmail.com>
2023-03-22 17:19:21 +0600

    passes all tsets.

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        if (sentence.isEmpty()) {
+            return map;
+        }
+        String[] words = sentence.split(" ");
+        for (String word:
+             words) {
+            word = word.toLowerCase();
+            if (word.contains(".") || word.contains(",")) {
+                word = word.substring(0, word.length() - 1);
+            }
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        return map;
     }
 }
